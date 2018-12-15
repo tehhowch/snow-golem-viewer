66f4a07958145df33e67ac0cc19862429bec7e75
tehhowch <tehhowch@users.noreply.github.com>
2018-12-14 19:06:00 -0600

    Remove "query twice" behavior

--- src/_API.js
+++ src/_API.js
@@ -1,32 +1,36 @@
 /**
  * Query the Horntracker API with the given JSON argument
  *
- * @param Object      A JSON object suitable to serve as a UrlFetchApp POST payload
- * @return Object     A JSON object, or null
+ * @param {Object.<string, string>} payload A JSON object suitable to serve as a UrlFetchApp POST payload
+ * @returns {Object.<string, any>|null} If there was no issue with the query (or HornTracker), returns the parsed JSON data. Otherwise, returns `null`
  */
-function readAPI_()
+function readAPI_(payload)
 {
-  var url = 'http://horntracker.com/backend/new/api.php';
-  if (arguments.length == 0) return null;
-  var o;
-  try
+  const url = 'http://horntracker.com/backend/new/api.php';
+  if (!payload)
+    return null;
+
+  // Formulate the request.
+  const rq = UrlFetchApp.getRequest(url, { 'method': 'POST', 'payload': payload });
+  try { var resp = UrlFetchApp.fetchAll([rq])[0]; }
+  catch (fetchError)
   {
-    o = JSON.parse(UrlFetchApp.fetch(url, { 'method': "POST", 'payload': arguments[0] }));
+    console.warn({ 'message': 'HornTracker query failed with error ' + fetchError, 'request': rq, 'response': resp });
+    return null;
   }
-  catch (e)
+
+  // If the server indicated an issue, don't try to parse the (likely invalid) data.
+  if (!resp || resp.getResponseCode() !== 200 || resp.getContentText().toLowerCase().indexOf('connect to'))
   {
-    // Perform the query twice, after waiting a bit (e.g. allow for Horntracker load issues).
-    console.warn({ message: 'First query failed. Attempting again after 5s', error: e });
-    Utilities.sleep(5000);
-    try
-    {
-      o = JSON.parse(UrlFetchApp.fetch(url, { 'method': "POST", 'payload': arguments[0] }));
-    }
-    catch (e)
-    {
-      console.error({ message: "Both queries failed.", error: e });
-      return null;
-    }
+    console.warn({ 'message': 'HornTracker server replied with bad data', 'request': rq, 'response': resp });
+    return null;
   }
-  return o;
+
+  // Parse the response.
+  try { return JSON.parse(resp.getContentText()); }
+  catch (parseError)
+  {
+    console.warn({ 'message': 'Received data was not JSON', 'request': rq, 'response': resp });
+    return null;
+  }
 }
